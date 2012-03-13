3dd940fa8334466b4327cf629692ae1f1eb16fe2
jrainbowmuffin@gmail.com <jrainbowmuffin@gmail.com@4fc87724-0331-c052-3676-68f9c1808678>
2012-03-13 17:18:30 +0000

    Changed to show a app friend picture instead of a dummy image

--- trunk/people-finder/People Finder/src/com/ece3574/dausin/activities/mapFinderActivity.java
+++ trunk/people-finder/People Finder/src/com/ece3574/dausin/activities/mapFinderActivity.java
@@ -1,8 +1,14 @@
 package com.ece3574.dausin.activities;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+//import java.util.ArrayList;
 import java.util.List;
 
 import com.ece3574.dausin.R;
+//import com.ece3574.dausin.global.Friend;
 import com.ece3574.dausin.maps.TheItemizedOverlay;
 
 import com.google.android.maps.GeoPoint;
@@ -10,13 +16,15 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
-
-//import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 public class mapFinderActivity extends MapActivity {
     /** Called when the activity is first created. */
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +35,38 @@ public class mapFinderActivity extends MapActivity {
         mapView.setBuiltInZoomControls(true);
         
         List<Overlay> mapOverlays = mapView.getOverlays();
-        Drawable drawable = this.getResources().getDrawable(R.drawable.robinh);
-        TheItemizedOverlay itemizedoverlay = new TheItemizedOverlay(drawable, this);
-    
-        GeoPoint point = new GeoPoint(19240000,-99120000);
-        OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
-    
-        itemizedoverlay.addOverlay(overlayitem);
-        mapOverlays.add(itemizedoverlay);
+        
+        /* Get picture from facebook to put on map*/
+        // TODO Not working correctly fix it UP!!!
+        Drawable drawable;
+		try {
+			drawable = drawableFromUrl(PeopleFinderActivity.getPracticeURL());
+	        TheItemizedOverlay itemizedoverlay = new TheItemizedOverlay(drawable, this);
+	        
+	        GeoPoint point = new GeoPoint(19240000,-99120000);
+	        OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+	    
+	        itemizedoverlay.addOverlay(overlayitem);
+	        mapOverlays.add(itemizedoverlay);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
+    
+    /* Function to convert URL images to drawables */
+    public static Drawable drawableFromUrl(String url) throws IOException {
+        Bitmap x;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.connect();
+        InputStream input = connection.getInputStream();
+
+        x = BitmapFactory.decodeStream(input);
+        return new BitmapDrawable(x);
+    }
+
+    
     // Don't display routes on the map
     @Override
     protected boolean isRouteDisplayed(){
