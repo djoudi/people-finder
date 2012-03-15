e31ad3bfab9d82e3e544bf153759dc7146acafa7
jbug4159 <jbug4159@4fc87724-0331-c052-3676-68f9c1808678>
2012-03-15 13:52:00 +0000

    morning of 3/15 again

---
+++ people-finder/People Finder/src/com/ece3574/dausin/activities/DialogActivity.java
@@ -0,0 +1,17 @@
+//package com.ece3574.dausin.activities;
+
+//import com.ece3574.dausin.R;
+
+//import android.app.Activity;
+//import android.os.Bundle;
+
+//public class DialogActivity extends Activity {
+//	public void onCreate(Bundle savedInstanceState){
+		
+//		super.onCreate(savedInstanceState);
+//		 setContentView(R.layout.dialogactivity);
+		 
+		 
+//	}
+
+//}
