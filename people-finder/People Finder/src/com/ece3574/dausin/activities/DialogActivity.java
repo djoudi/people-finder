c3aea6668ca5cf751cbe01f15afcfdb61d081b54
jbug4159 <jbug4159@4fc87724-0331-c052-3676-68f9c1808678>
2012-03-29 00:12:06 +0000

    fixed crash

--- people-finder/People Finder/src/com/ece3574/dausin/activities/DialogActivity.java
+++ people-finder/People Finder/src/com/ece3574/dausin/activities/DialogActivity.java
@@ -6,61 +6,31 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-
-
 
 public class DialogActivity extends Activity {
 	
 	private Button accepting;
 	private Button ignoring;
-	private TextView header, body;
     private String[] str;
     private String delimiter = ":";
-    private String dBody = "This person has requested to seek you using People Finder." +
-			"Would you like to let them?";
 	
 	public void onCreate(Bundle savedInstanceState){
-
 		
-
 		super.onCreate(savedInstanceState);
-		 setContentView(R.layout.receiverdialog);
-		 //final String id = compound.substring(0, compound.indexOf(":"));
-		 //header = (TextView) findViewById(R.id.header);
-		 //header.setText(name);
-
-
-		accepting = (Button) findViewById(R.id.receiverAccept);
-		accepting.setText("Accept");
-		
-		ignoring = (Button) findViewById(R.id.receiverIgnore);
-		ignoring.setText("Ignore");
-		
-
+		 setContentView(R.layout.dialogactivity);
+		 
+		accepting = (Button) findViewById(R.id.button2);
+		ignoring = (Button) findViewById(R.id.button1);
 
 		 Bundle extras = getIntent().getExtras();
-		 String idAndNumberAndName = extras.getString("ID_RNUM");
+		 String idAndReturnNumber = extras.getString("ID_RNUM");
 		 //String returnNumber = extras.getString("RETURN_NUMBER");
 
-         str = idAndNumberAndName.split(delimiter);
-
-		 //body = (TextView) findViewById(R.id.body);
-		 //body.setText(dBody);
-         
- 		header = (TextView) findViewById(R.id.header);
- 		header.setText(str[3]);
- 		
- 		body = (TextView) findViewById(R.id.body);
- 		body.setText(dBody);
-
+         str = idAndReturnNumber.split(delimiter);
 		 
-
 		 Toast.makeText(getApplicationContext(), str[1], Toast.LENGTH_LONG).show();
 		 
 		 accepting.setOnClickListener(new View.OnClickListener()
@@ -77,41 +47,4 @@ public class DialogActivity extends Activity {
 		 
 	}
 
-		 //accept = (Button) findViewById(R.id.receiverAccept);
-		 //accept.setText("Accept");
-		 //accept.setOnClickListener(new OnClickListener(){
 }
-
-/*
-			@Override
-			public void onClick(View arg0) {
-				
-				PeopleFinderActivity.currentTag = id;
-				Intent i = new Intent(DialogActivity.this, mapFinderActivity.class);
-	        	startActivity(i);
-				
-			}
-			 
-		 });
-		 
-		 ignore = (Button) findViewById(R.id.receiverIgnore);
-		 ignore.setText("Ignore");
-		 ignore.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				
-				finish();
-			}
-			 
-		 });
-
-		 
-		 
-		 
-		 
-	}
-
-}
-*/
-
